aa1026ead154ad7e44bf440797852710e4c41581
taektu_coding <taektu.coding@gmail.com>
2025-03-11 17:38:19 +0100

    added: user-logo with first letter of name and lastname + random color

--- src/app/maincontent/contacts/contactlist/contactlist.component.ts
+++ src/app/maincontent/contacts/contactlist/contactlist.component.ts
@@ -10,4 +10,17 @@ import { FirebaseService } from '../../../shared/services/firebase.service';
 })
 export class ContactlistComponent {
   firebaseService = inject(FirebaseService);
+
+  getRandomColor(index: number): string {
+    const colors = [
+      '#FF5733',
+      '#33FF57',
+      '#3357FF',
+      '#FF33A5',
+      '#F3FF33',
+      '#33FFF7',
+      '#FF8333',
+    ];
+    return colors[index % colors.length]; // Rotate through colors based on index
+  }
 }
