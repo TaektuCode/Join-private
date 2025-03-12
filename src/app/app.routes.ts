3b6e85fded554a91d743e584a9ec82e539ba2905
MichaelS96 <m.schwittay96@web.de>
2025-03-12 10:19:28 +0100

    routerlink for imprint/policy added in footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MaincontentComponent } from './maincontent/maincontent.component';
+import { PolicyComponent } from './policy/policy.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
     { path: '', component: MaincontentComponent },
+    { path: 'policy', component: PolicyComponent },
+    { path: 'imprint', component: ImprintComponent }
 ];
