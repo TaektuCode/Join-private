b120e4abc802e7e8ab4194992731c381a17599fe
MichaelS96 <m.schwittay96@web.de>
2025-03-12 10:36:04 +0100

    routerlink added for all 4sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { MaincontentComponent } from './maincontent/maincontent.component';
 import { PolicyComponent } from './policy/policy.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { SummaryComponent } from './maincontent/summary/summary.component';
+import { AddtaskComponent } from './maincontent/addtask/addtask.component';
+import { BoardComponent } from './maincontent/board/board.component';
+import { ContactsComponent } from './maincontent/contacts/contacts.component';
 
 export const routes: Routes = [
     { path: '', component: MaincontentComponent },
     { path: 'policy', component: PolicyComponent },
-    { path: 'imprint', component: ImprintComponent }
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'summary', component: SummaryComponent },
+    { path: 'addtask', component: AddtaskComponent },
+    { path: 'board', component: BoardComponent },
+    { path: 'contacts', component: ContactsComponent }
 ];
