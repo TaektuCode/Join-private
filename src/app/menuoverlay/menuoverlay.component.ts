d35b5b127201e346855a8541720a0390a27d0184
Ray <rainer.musch@gmx.de>
2025-03-13 15:24:34 +0100

    udate responsive menuoverlay

--- src/app/menuoverlay/menuoverlay.component.ts
+++ src/app/menuoverlay/menuoverlay.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menuoverlay',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './menuoverlay.component.html',
   styleUrl: './menuoverlay.component.scss'
 })
 export class MenuoverlayComponent {
+  @Input() visible = false;
+  @Output() closed = new EventEmitter<void>();
+
+  close(): void {
+    this.visible = false;
+    this.closed.emit();
+  }
 
 }
