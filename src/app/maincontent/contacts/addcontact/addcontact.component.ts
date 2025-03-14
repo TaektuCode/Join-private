2692b286816b8c49145ad1cc2ac69da7bd180067
taektu_coding <taektu.coding@gmail.com>
2025-03-14 07:16:42 +0100

    added: form validation, different svgs, exit button on add contact form

--- src/app/maincontent/contacts/addcontact/addcontact.component.ts
+++ src/app/maincontent/contacts/addcontact/addcontact.component.ts
@@ -1,7 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { FirebaseService } from '../../../shared/services/firebase.service';
 import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-addcontact',
   standalone: true,
@@ -10,17 +17,22 @@ import { addDoc, collection, Firestore } from '@angular/fire/firestore';
   styleUrl: './addcontact.component.scss',
 })
 export class AddcontactComponent {
-  firebaseService = inject(FirebaseService); // Inject FirebaseService
-  firestore = inject(Firestore); // Inject Firestore
+  firebaseService = inject(FirebaseService);
+  firestore = inject(Firestore);
+  fb = inject(FormBuilder); // Inject FormBuilder
 
-  applyForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    color: new FormControl(''),
-  });
+  applyForm: FormGroup; // Deklariere applyForm als FormGroup
 
   showOverlay = false;
+  formSubmitted = false;
+
+  constructor() {
+    this.applyForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.pattern(/^\+?\d{1,15}$/)]],
+    });
+  }
 
   openOverlay(): void {
     this.showOverlay = true;
@@ -29,14 +41,15 @@ export class AddcontactComponent {
   closeOverlay(): void {
     this.showOverlay = false;
     this.applyForm.reset();
+    this.formSubmitted = false;
   }
 
   async submitAddContact() {
+    this.formSubmitted = true;
     if (this.applyForm.valid) {
       const contactData = this.applyForm.value;
-      const randomColor = this.getRandomColor(); // Generate a random color
+      const randomColor = this.getRandomColor();
 
-      // Create a new object with the contact data and the random color
       const contactWithColor = {
         name: contactData.name,
         email: contactData.email,
@@ -48,7 +61,19 @@ export class AddcontactComponent {
       console.log('Contact added successfully!');
       this.closeOverlay();
       this.applyForm.reset();
+    } else {
+      this.formError(this.applyForm); // Fehler anzeigen
     }
+  }
+
+  formError(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control) => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.formError(control);
+      }
+    });
   }
 
   getRandomColor(): string {
