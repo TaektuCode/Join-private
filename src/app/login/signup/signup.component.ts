97dbef6ea329759174f4ddf0efadf5162d9946a2
Ray <rainer.musch@gmx.de>
2025-03-17 06:32:32 +0100

    add routerlink to links in the header

--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
