96c7bd26fe92005ed69501d6a85c95244c3fb228
taektu_coding <taektu.coding@gmail.com>
2025-03-18 17:00:03 +0100

    fix: form validation on editContact + responisve fixes

--- src/app/maincontent/contacts/editcontact/editcontact.component.ts
+++ src/app/maincontent/contacts/editcontact/editcontact.component.ts
@@ -31,22 +31,16 @@ export class EditcontactComponent implements OnInit {
   @Output() editFinished = new EventEmitter<void>();
 
   applyForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl('', [Validators.email]),
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]), // Füge Validierung für Name hinzu
+    email: new FormControl('', [Validators.required, Validators.email]), // Füge Required-Validator hinzu
     phone: new FormControl('', [Validators.pattern(/^[\+]?[0-9]+$/)]),
   });
 
   showOverlay = false;
   contactInitials: string = '';
+  formSubmitted = false; // Deklariere formSubmitted
 
   ngOnInit(): void {
-    if (this.contact) {
-      this.contactInitials = this.getInitials(this.contact.name);
-    }
-  }
-
-  editContactShowOverlay(): void {
-    this.showOverlay = true;
     if (this.contact) {
       this.contactInitials = this.getInitials(this.contact.name);
       this.applyForm.patchValue({
@@ -57,13 +51,35 @@ export class EditcontactComponent implements OnInit {
     }
   }
 
+  editContactShowOverlay(): void {
+    this.showOverlay = true;
+    this.formSubmitted = false; // Setze formSubmitted beim Öffnen zurück
+    if (this.contact) {
+      this.contactInitials = this.getInitials(this.contact.name);
+      this.applyForm.patchValue({
+        name: this.contact.name,
+        email: this.contact.email,
+        phone: this.contact.phone ? this.contact.phone.toString() : '',
+      });
+      // Optional: Setze Validierungs- und Touch-Status zurück
+      this.applyForm.markAsUntouched();
+      this.applyForm.updateValueAndValidity();
+    }
+  }
+
+  getErrorTopPosition(index: number): number {
+    return 55 + index * 85;
+  }
+
   closeOverlay(): void {
     this.showOverlay = false;
     this.applyForm.reset();
+    this.formSubmitted = false; // Setze formSubmitted beim Schließen zurück
     this.editFinished.emit();
   }
 
   async submitEditContact() {
+    this.formSubmitted = true; // Setze formSubmitted auf true beim Absenden
     if (this.applyForm.valid && this.contact && this.contact.id) {
       const contactData = this.applyForm.value;
 
@@ -71,7 +87,7 @@ export class EditcontactComponent implements OnInit {
         name: contactData.name,
         email: contactData.email,
         phone: contactData.phone,
-        color: this.contact.color, // Use the original color
+        color: this.contact.color, // Verwende die ursprüngliche Farbe
       };
 
       const contactDocRef = doc(this.firestore, 'contacts', this.contact.id);
