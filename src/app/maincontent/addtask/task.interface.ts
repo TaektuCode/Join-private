91f988a7c1463607bb20a9a6d1bfa351225399d7
taektu_coding <taektu.coding@gmail.com>
2025-03-19 16:08:36 +0100

    added: task interface

---
+++ src/app/maincontent/addtask/task.interface.ts
@@ -0,0 +1,12 @@
+export interface TaskInterface {
+  id?: string; // Optional, von Firebase generiert
+  assignedTo?: string[];
+  category: string; // Nicht optional
+  created?: Date;
+  date: Date; // Nicht optional
+  description?: string;
+  edited?: Date;
+  priority?: 'urgent' | 'medium' | 'low';
+  subtask?: { title: string; completed: boolean }[]; // Oder Subtask-Objekt-Array mit status erledigt oder nicht
+  title: string; // Nicht optional
+}
