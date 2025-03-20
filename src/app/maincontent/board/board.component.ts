b2b8af2d52413d423689ed87f9e10cc64b07b359
Ray <rainer.musch@gmx.de>
2025-03-20 07:19:08 +0100

    add Drag and Drop List for testing

--- src/app/maincontent/board/board.component.ts
+++ src/app/maincontent/board/board.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList, } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
