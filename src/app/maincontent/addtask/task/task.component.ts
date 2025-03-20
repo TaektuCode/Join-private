2e6bade797c342785bcfb777babd4e837b46a09e
taektu_coding <taektu.coding@gmail.com>
2025-03-20 10:30:26 +0100

    setup task html

--- src/app/maincontent/addtask/task/task.component.ts
+++ src/app/maincontent/addtask/task/task.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
-
+  isClicked = false;
 }
