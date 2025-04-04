964e915f1f95f19d903da421a162cdd757195cd5
Ray <rainer.musch@gmx.de>
2025-04-04 09:58:44 +0200

    add auth to header compoment

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,20 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuoverlayComponent } from '../../menuoverlay/menuoverlay.component';
+import { AuthService } from '../../login/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule, MenuoverlayComponent],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'] 
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   overlayVisible = false;
+  userInitials = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const user = this.authService.getCurrentUser(); // Beispielhafter Aufruf
+    if (user && user.name) {
+      this.userInitials = this.getUserInitials(user.name);
+    }
+  }
+
+  getUserInitials(fullName: string): string {
+    const [first, second] = fullName.trim().split(' ');
+    let initials = first.charAt(0);
+    if (second) {
+      initials += second.charAt(0);
+    }
+    return initials.toUpperCase();
+  }
 
   toggleOverlay(event: MouseEvent): void {
     event.stopPropagation();
     this.overlayVisible = !this.overlayVisible;
   }
 }
+
+
 
