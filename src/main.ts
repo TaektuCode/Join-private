1545d2133fed927e2269fb0c5ea84125a642ba08
taektu_coding <taektu.coding@gmail.com>
2025-06-24 11:46:04 +0200

    deplox testing

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 /**
  * Bootstraps the Angular application by providing the root component (AppComponent)
  * and the application configuration (appConfig). Any errors during the bootstrapping
  * process are caught and logged to the console.
  */
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+console.log('Firebase Config being used:', environment.firebase);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
